cc952a85b59881986f5ee8802a4c0fd4db7f8403
ironweb-research <backslash.with.n@gmail.com>
2024-11-01 12:28:19 +0800

    remvoe generate-sitemap.js, tidy sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,3 +1,4 @@
+const fs = require("fs");
 import { getAllPosts } from "@/lib/api";
 import { BASE_URL, REPO_NAME } from "@/lib/constants";
 
@@ -7,9 +8,21 @@ export default async function sitemap() {
   const posts = getAllPosts();
 
   const routes = posts.map((post) => ({
-    url: `${BASE_URL}${REPO_NAME}/posts/"${post.slug.replace('.{js,jsx,ts,tsx,mdx}',"")}`,
+    url: `${BASE_URL}${REPO_NAME}/posts/${post.slug.replace('.{js,jsx,ts,tsx,mdx}',"")}`,
     lastModified: new Date().toISOString(),
   }));
   
   return [...routes]; // spread operations on [routes]
 }
+
+async function generateSitemap() {
+  const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <?xml version="1.0" encoding="UTF-8"?> 
+  lastModified: new Date().format(page.updated_at, 'dd-MMM-yyyy hh:mm:ss'),
+  </urlset>`;
+  fs.writeFileSync("public/sitemap.xml", sitemap);
+}
+
+generateSitemap();
+
+
