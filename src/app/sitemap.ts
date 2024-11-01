59c17f3fca0ff8e2d565af19552200e4b42e9222
ironweb-research <backslash.with.n@gmail.com>
2024-11-01 12:41:57 +0800

    tidy sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,4 @@
-const fs = require("fs");
+// const fs = require("fs");
 import { getAllPosts } from "@/lib/api";
 import { BASE_URL, REPO_NAME } from "@/lib/constants";
 
@@ -14,15 +14,5 @@ export default async function sitemap() {
   
   return [...routes]; // spread operations on [routes]
 }
-
-async function generateSitemap() {
-  const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <?xml version="1.0" encoding="UTF-8"?> 
-  lastModified: new Date().format(page.updated_at, 'dd-MMM-yyyy hh:mm:ss'),
-  </urlset>`;
-  fs.writeFileSync("public/sitemap.xml", sitemap);
-}
-
-generateSitemap();
 
 
