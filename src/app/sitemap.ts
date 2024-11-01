974a0c5d13fdba0067dab03a691bcb7255febeec
ironweb-research <backslash.with.n@gmail.com>
2024-11-01 09:41:21 +0800

    refine workflow - steps.deployment.outputs.page_url and sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ import { url } from "inspector";
 export const revalidate = 3600; // hourly revalidation
 
 export default async function sitemap() {
-  const posts = getAllPosts();
+  const posts = await getAllPosts();
 
   // const pages = await globby([
   //   "src/app/**/*{.js,jsx,ts,tsx,.mdx}",
@@ -12,17 +12,28 @@ export default async function sitemap() {
   //   "!src/app/{sitemap,layout}.{js,jsx,ts,tsx}",
   //   "!src/app/api",
   // ]);
+
+  // const cleanurls = posts
+
+  // cleanurls.map(cleanURL);
+
+  // function cleanURL(item:any){
+  //   return [item].;
+  // }
+
   const routes = posts.map((post) => ({
-    url: `${process.env.WEBSITE_URL}${post.slug}.replace(/\.md$/, ""))`,
+    realSlug : post.slug.replace(/\.md$/, ""),
+    url: `${process.env.WEBSITE_URL}/${post.slug.replace('.md',"")}`,
     lastModified: new Date().toISOString(),
   }));
-
-  console.log(`url per post                  >>>>>>>>>>>>>>>>: ${url}`)
+  
+  // url: `${process.env.WEBSITE_URL}${post.slug}.replace(/\.md$/, ""))`,
+  console.log(`process.env.WEBSITE_URL       >>>>>>>>>>>>>>>>: ${process.env.WEBSITE_URL}`)
+  console.log(`url per post                  >>>>>>>>>>>>>>>>: ${routes[0].realSlug}`)
   console.log(`routes                        >>>>>>>>>>>>>>>>: ${{routes}}`)
 
   // routes                        >>>>>>>>>>>>>>>>: [object Object]
 
   // pages                         >>>>>>>>>>>>>>>>: src/app/page.tsx,src/app/contact/page.tsx, \
-  console.log(`process.env.WEBSITE_URL       >>>>>>>>>>>>>>>>: ${process.env.WEBSITE_URL}`)
   return [...routes]; // spread operator on [routes]
 }
