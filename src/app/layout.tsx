7464f891b5d991aeae74f4063773f70e4536d2b8
ironweb-research <backslash.with.n@gmail.com>
2024-11-05 18:21:56 +0800

    mass clean up website_hosting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import Footer from "@/app/_components/footer";
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
+import { BASE_URL, IRONWEB_ANI_GIF_URL } from "@/lib/constants";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import cn from "classnames";
@@ -16,15 +16,8 @@ export const metadata = {
   openGraph: {
     title: 'Ironweb-Research ~ Future is Secure',
     description: 'Our Startup aims to integrate blockchain technology to secure networking systems.',
-    images: [HOME_OG_IMAGE_URL],
+    images: [IRONWEB_ANI_GIF_URL],
   },
-  // telegram: {
-  //   card: 'summary_large_image',
-  //   site: '@tkokhing',
-  //   title: 'Ironweb-Research ~ Future is Secure',
-  //   description: 'this is the desciption',
-  //   image: 'url/image.png'
-  // }
 }
 
 export default function RootLayout({
@@ -37,35 +30,35 @@ export default function RootLayout({
       <head>
         <link
           rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
+          sizes="180x200"
+          href={`${BASE_URL}/assets/logo/apple-touch-icon.png`} 
         />
         <link
           rel="icon"
           type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
+          sizes="32x35"
+          href={`${BASE_URL}/assets/logo/ironweb_icon_32x35.png`} 
         />
         <link
           rel="icon"
           type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
+          sizes="16x17"
+          href={`${BASE_URL}/assets/logo/ironweb_icon_16x17.png`} 
         />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="manifest" href={`${BASE_URL}/assets/logo/site.webmanifest`}/>
         <link
           rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
+          href={`${BASE_URL}/assets/logo/safari-pinned-tab.svg`} 
           color="#000000"
         />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="shortcut icon" href={`${BASE_URL}/assets/logo/ironweb_icon.ico`}  />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta
           name="msapplication-config"
-          content="/favicon/browserconfig.xml"
+          content={`${BASE_URL}/assets/logo/browserconfig.xml`} 
         />
         <meta name="theme-color" content="#000" />
-        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+        <link rel="alternate" type="application/rss+xml" href={`${BASE_URL}/feed.xml`}/>
       </head>
       <body
         className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}
